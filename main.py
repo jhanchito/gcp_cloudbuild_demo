2c7787c83f6d2f4bc3ff0465af18b061c69bc29d
jhampier <jhampiertapia@gmail.com>
2025-10-08 12:12:42 +0000

    committed

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def main():
-    model = {"title": "Hello Build Trigger."}
+    model = {"title": "Hello Build Trigger. JHAMPIER"}
     return render_template("index.html", model=model)
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
